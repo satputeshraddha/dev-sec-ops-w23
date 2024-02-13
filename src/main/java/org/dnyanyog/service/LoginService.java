4df375d1e4fb63359f88e1b4c01b0fde33f01599
satputeshraddha <shraddhasatpute2@gmail.com>
2024-02-13 20:27:41 +0530

    Initial commit

---
+++ src/main/java/org/dnyanyog/service/LoginService.java
@@ -0,0 +1,12 @@
+package org.dnyanyog.service;
+
+import org.dnyanyog.dto.LoginRequest;
+import org.dnyanyog.dto.LoginResponse;
+import org.springframework.stereotype.Service;
+@Service
+public interface LoginService {
+
+
+    public LoginResponse validateUser(LoginRequest loginRequest);
+
+    }
